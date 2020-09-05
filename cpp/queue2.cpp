c51a82266f807e4431dcacb1b062cc81f9b35926
Fmagellan <fmagellan1519@gmail.com>
2020-09-05 20:45:15 +0530

    Wrote code changes for a Circular Queue ADT, using arrays.

---
+++ cpp/queue2.cpp
@@ -0,0 +1,45 @@
+// Copyright 2020 Magellan
+
+#include "cpp/queueArrayTemplate.h"
+
+int main() {
+    Magellan::Queue<int> queue(10);
+
+    queue.push_back(0);
+    queue.push_back(1);
+    queue.push_back(2);
+    queue.push_back(3);
+    queue.push_back(4);
+    queue.push_back(5);
+    queue.push_back(6);
+    queue.push_back(7);
+    queue.push_back(8);
+
+    std::cout << queue;
+
+    queue.pop_front();
+    std::cout << queue;
+
+    queue.push_back(9);
+    std::cout << queue;
+
+    queue.push_back(10);
+    std::cout << queue;
+
+    queue.push_back(11);
+    std::cout << queue;
+
+    queue.push_back(12);
+    std::cout << queue;
+
+    queue.pop_front();
+    std::cout << queue;
+
+    queue.pop_front();
+    std::cout << queue;
+
+    queue.push_back(13);
+    std::cout << queue;
+
+    return (0);
+}
---
+++ cpp/queueArrayTemplate.h
@@ -0,0 +1,92 @@
+// Copyright 2020 Magellan
+
+// This file implements a circular queue ADT, using an array.
+
+#ifndef CPP_QUEUEARRAYTEMPLATE_H_
+#define CPP_QUEUEARRAYTEMPLATE_H_
+
+#include <cassert>
+#include <iostream>
+
+namespace Magellan {
+
+template <class T>
+class Queue {
+    private:
+        int m_size{};
+        int m_front{ -1 };
+        int m_back{ -1 };
+        T *m_array{};
+
+    public:
+        explicit Queue(const int size = 100)
+            : m_size{ size }, m_front{ -1 }, m_back{ -1 }, m_array{ nullptr } {
+            if (size) {
+                m_array = new T[size];
+            }
+        }
+
+        ~Queue() {
+            if (m_size) {
+                delete [] m_array;
+            }
+        }
+
+        void push_back(const T &item);
+        T pop_front();
+        bool isEmpty() const;
+        bool isFull() const;
+
+        friend std::ostream& operator<< (std::ostream &out, const Queue &queue) {
+            out << "Queue: ";
+            if (queue.isEmpty()) {
+                out << '\n';
+
+                return out;
+            }
+
+            for (int index{ queue.m_front + 1 }; index != queue.m_back + 1; ++index) {
+                index %= queue.m_size;
+                out << queue.m_array[index] << ' ';
+            }
+
+            out << '\n';
+
+            return out;
+        }
+};
+
+template <class T>
+void Queue<T>::push_back(const T &item) {
+    if (isFull()) {
+        // if the Queue is full, silently return.
+        return;
+    }
+
+    ++m_back;
+    m_back %= m_size;
+    m_array[m_back] = item;
+}
+
+template <class T>
+T Queue<T>::pop_front() {
+    assert((m_front != m_back) && "Queue is empty");
+
+    ++m_front;
+    m_front %= m_size;
+    return (m_array[m_front]);
+}
+
+template <class T>
+bool Queue<T>::isEmpty() const {
+    return (m_front == m_back);
+}
+
+template <class T>
+bool Queue<T>::isFull() const {
+    return (((m_back + 1) % m_size) == m_front);
+}
+
+};  // namespace Magellan
+
+#endif  // CPP_QUEUEARRAYTEMPLATE_H_
